32ab3958381171db538bda3f70ca2619e37e2b3e
xi <xi@dev.com>
2023-01-09 16:23:17 +0800

    fix

--- util/httputil/result.go
+++ util/httputil/result.go
@@ -39,12 +39,12 @@ func Error(err error) Result[interface{}] {
 }
 
 func OutResult[T any](w http.ResponseWriter, result Result[T]) error {
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
 	if result.Code == ResultCodeSuccess {
 		w.WriteHeader(200)
 	} else {
 		w.WriteHeader(400)
 	}
-	w.Header().Add("Content-Type", "application/json; charset=utf-8")
 	b, err := json.Marshal(result)
 	if err != nil {
 		return err
